3da4aba103d817a4651f6836c771830b5037d7d7
itsrahulsehgal <rahul.sehgal049@gmail.com>
2023-10-02 14:27:48 +0530

    Mongo uri error resolved

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 const port = process.env.PORT || 3000
 const corsOptions = {
-  origin:['http://localhost:3001','http://localhost:3000',
+  origin:['http://localhost:3000',
   'https://task-manager-app-x4qo.vercel.app/'], 
   methods : ["POST","GET"],
   credentials: true, 
